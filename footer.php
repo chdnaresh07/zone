ea8861e8a401a0e8d173465d3e640760502e2328
chdnaresh07 <81208502+chdnaresh07@users.noreply.github.com>
2021-03-23 13:17:10 +0530

    this is the second site

---
+++ footer.php
@@ -0,0 +1,16 @@
+			<br /><br /></div>
+		</div>
+		<font size="4" style="background-color:#000000 "<div id="footer-wrapper">
+			<div class="container">
+				<div class="row">
+					<div class="12u">
+						<div id="copyright">
+							<a href="#">&copy; 2013 - <?php echo date("Y"); ?> www.computerzone.online</a> All rights Reserved. | Site make by: <a href="home_admin.php" target="_blank"> &copy;  Naresh Chaudhary</a> 
+						</div>
+					</div>
+				</div>
+			</div>
+		</div></font></center>
+	
+	</body>
+</html>
